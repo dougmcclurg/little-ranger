e5c380723057c3c9b269dbdb14b3772c842ed970
Douglas McClurg <doug.mcclurg@gmail.com>
2014-04-22 22:18:20 -0400

    First push

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Grunt Bootstrap
+Little Ranger
 
 # Setup
 
--- public/javascript/templates.js
+++ public/javascript/templates.js
@@ -1,11 +1,11 @@
-angular.module('bootstrap').run(['$templateCache', function($templateCache) {
+angular.module('little-ranger').run(['$templateCache', function($templateCache) {
   'use strict';
 
   $templateCache.put('app/views/index.html',
     "<!doctype html>\n" +
     "<html class=\"no-js\">\n" +
     "  <head>\n" +
-    "      <title>Bootstrap</title>\n" +
+    "      <title>Little Ranger</title>\n" +
     "      <meta charset=\"utf-8\">\n" +
     "      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n" +
     "      <link rel=\"shortcut icon\" href=\"/favicon.ico\">\n" +
@@ -14,7 +14,7 @@ angular.module('bootstrap').run(['$templateCache', function($templateCache) {
     "      <script src=\"vendor/modernizr/modernizr.js\"></script>\n" +
     "  </head>\n" +
     "  <body>\n" +
-    "    <h1>BOOTSTRAP</h1>\n" +
+    "    <h1>Little Ranger</h1>\n" +
     "  </body>\n" +
     "  <script src=\"vendor/angular/angular.js\"></script>\n" +
     "  <script src=\"vendor/angular-ui-router/angular-ui-router.js\"></script>\n" +
